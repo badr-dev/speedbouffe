93397ca1fb14cb0393099c6a31574e0b61b75446
Ronenn MAZOUZ <ronenn.ma@gmail.com>
2017-03-20 17:13:59 +0100

    Remove unused and changes on ajaxget

--- js/ajaxget.js
+++ js/ajaxget.js
@@ -2,20 +2,7 @@
  * Ajax GET methods
  */
 $(document).ready(function () {
-
-	/**
-	 * Orders from generateur_speedbouffe
-	 */
-	$.get(
-		'../data/generateur_speedbouffe/script.php',
-		'false',
-		'getAllOrders',
-		'json'
-	);
-
-	function getAllOrders(data){
-		console.log("TEST");
+	$.get("../data/generateur_speedbouffe/script.php", function(data) {
 		console.log(data);
-	}
-
+	});
 });
