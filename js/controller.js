92dc8afdf8548fc157f38396b0988e9f03a703eb
Ronenn MAZOUZ <ronenn.ma@gmail.com>
2017-03-27 10:11:40 +0200

    Avancement detail commande

--- js/controller.js
+++ js/controller.js
@@ -249,53 +249,52 @@ app.controller('detailCommandeCtrl', function ($scope, $route, $routeParams) {
 
 	// TODO Get sur service commande/:id
 
-	$scope.commande =
-		{
-			"Acheteur": {
-				"Civilite": "Mademoiselle",
-				"Nom": "Vallie",
-				"Prenom": "Elvis",
-				"Age": 18,
-				"Email": "vallie.elvis@e-corp.com"
-			},
-			"Infos_commande": {
-				"Jour": "2017-02-20",
-				"Horaire_livraison": "08:00",
-				"Paiement_espece": "Non"
-			},
-			"Details_commande": [
-				{
-					"Commande1": {
-						"Repas": "Les trois petits cochons",
-						"Civilite": "Mademoiselle",
-						"Nom": "Vallie",
-						"Prenom": "Elvis",
-						"Age": 18,
-						"Tarif": "Etudiant"
-					}
-				},
-				{
-					"Commande2": {
-						"Repas": "Flageollet fatal",
-						"Civilite": "Monsieur",
-						"Nom": "Roscoe",
-						"Prenom": "Edwardo",
-						"Age": 14,
-						"Tarif": "Etudiant"
-					}
-				},
-				{
-					"Commande3": {
-						"Repas": "Fruits défendus",
-						"Civilite": "Mademoiselle",
-						"Nom": "Federico",
-						"Prenom": "Leora",
-						"Age": 60,
-						"Tarif": "Senior"
-					}
+	$scope.commande = {
+		"Acheteur": {
+			"Civilite": "Mademoiselle",
+			"Nom": "Vallie",
+			"Prenom": "Elvis",
+			"Age": 18,
+			"Email": "vallie.elvis@e-corp.com"
+		},
+		"Infos_commande": {
+			"Jour": "2017-02-20",
+			"Horaire_livraison": "08:00",
+			"Paiement_espece": "Non"
+		},
+		"Details_commande": [
+			{
+				"Commande1": {
+					"Repas": "Les trois petits cochons",
+					"Civilite": "Mademoiselle",
+					"Nom": "Vallie",
+					"Prenom": "Elvis",
+					"Age": 18,
+					"Tarif": "Etudiant"
 				}
-			]
-		};
+			},
+			{
+				"Commande2": {
+					"Repas": "Flageollet fatal",
+					"Civilite": "Monsieur",
+					"Nom": "Roscoe",
+					"Prenom": "Edwardo",
+					"Age": 14,
+					"Tarif": "Etudiant"
+				}
+			},
+			{
+				"Commande3": {
+					"Repas": "Fruits défendus",
+					"Civilite": "Mademoiselle",
+					"Nom": "Federico",
+					"Prenom": "Leora",
+					"Age": 60,
+					"Tarif": "Senior"
+				}
+			}
+		]
+	};
 
 });
 
