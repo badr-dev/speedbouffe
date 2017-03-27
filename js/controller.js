9b329c2e0eaca6b1abbdd31ea4a029c50f154217
Ronenn MAZOUZ <ronenn.ma@gmail.com>
2017-03-27 10:01:34 +0200

    Avancement detail commande

--- js/controller.js
+++ js/controller.js
@@ -246,6 +246,58 @@ app.controller('commandesCtrl', function ($scope) {
 app.controller('detailCommandeCtrl', function ($scope, $route, $routeParams) {
 
 	$scope.commandeId = $routeParams.id;
+
+	// TODO Get sur service commande/:id
+
+	$scope.commande = {
+		1: {
+			"Acheteur": {
+				"Civilite": "Mademoiselle",
+				"Nom": "Vallie",
+				"Prenom": "Elvis",
+				"Age": 18,
+				"Email": "vallie.elvis@e-corp.com"
+			},
+			"Infos_commande": {
+				"Jour": "2017-02-20",
+				"Horaire_livraison": "08:00",
+				"Paiement_espece": "Non"
+			},
+			"Details_commande": [
+				{
+					"Commande1": {
+						"Repas": "Les trois petits cochons",
+						"Civilite": "Mademoiselle",
+						"Nom": "Vallie",
+						"Prenom": "Elvis",
+						"Age": 18,
+						"Tarif": "Etudiant"
+					}
+				},
+				{
+					"Commande2": {
+						"Repas": "Flageollet fatal",
+						"Civilite": "Monsieur",
+						"Nom": "Roscoe",
+						"Prenom": "Edwardo",
+						"Age": 14,
+						"Tarif": "Etudiant"
+					}
+				},
+				{
+					"Commande3": {
+						"Repas": "Fruits défendus",
+						"Civilite": "Mademoiselle",
+						"Nom": "Federico",
+						"Prenom": "Leora",
+						"Age": 60,
+						"Tarif": "Senior"
+					}
+				}
+			]
+		}
+	};
+
 });
 
 app.controller('statsCtrl', function ($scope) {
